b707a3a1a6856768cb6be15550c003782c0670d3
sammeboy635 <samueltoddwaite@gmail.com>
2024-02-11 21:32:04 -0700

    almost created a cube

--- src/lib.rs
+++ src/lib.rs
@@ -36,21 +36,50 @@ impl Vertex {
     }
 }
 
+// const VERTICES: &[Vertex] = &[
+//     // Changed
+//     Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
+//     Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
+//     Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
+//     Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
+//     Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+// ];
+
+// const INDICES: &[u16] = &[
+//     0, 1, 4,
+//     1, 2, 4,
+//     2, 3, 4,
+// ];
+ //0
+//1
+ //2
+//3
+ //4
+//5
+ //6
+//7
 const VERTICES: &[Vertex] = &[
-    // Changed
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+	Vertex { position: [1.0,  1.0, -1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [-1.0,  1.0, -1.0], tex_coords: [0.0, 0.0] },
+    Vertex { position: [-1.0, -1.0, -1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0, -1.0], tex_coords: [1.0, 1.0] },
+
+    Vertex { position: [ 1.0,  1.0,  1.0], tex_coords: [1.0, 0.0] },
+    Vertex { position: [-1.0,  1.0,  1.0], tex_coords: [0.0, 0.0] },
+    Vertex { position: [-1.0, -1.0,  1.0], tex_coords: [0.0, 1.0] },
+    Vertex { position: [ 1.0, -1.0,  1.0], tex_coords: [1.0, 1.0] },
+ 
 ];
 
 const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+	// Front face
+	0,2,1,  0,3,2,
+	4,3,0,  4,7,3,
+	4,1,5,  4,0,1,
+	3,6,2,  3,7,6,
+	1,6,5,  1,2,6,
+	7,5,6,  7,4,5,
 ];
-
 
 
 pub async fn run() {
@@ -291,6 +320,7 @@ impl State {
 		});
 
 		// Camera End
+		
 		let modes = &surface_caps.present_modes;
 
 		let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
