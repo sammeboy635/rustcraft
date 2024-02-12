a2651d046c35fe5a4a0989f35a1740f4aabd44f7
sammeboy635 <samueltoddwaite@gmail.com>
2024-02-11 17:35:08 -0700

    before Vertex

---
+++ src/lib.rs
@@ -0,0 +1,277 @@
+use winit::event;
+use winit::window::Window;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+ 	let mut state = State::new(window).await;
+
+	 event_loop.run(move |event, _, control_flow| {
+		match event {
+			Event::WindowEvent {
+				ref event,
+				window_id,
+			} if window_id == state.window().id() => if !state.input(event, control_flow) { 
+				match event {
+					WindowEvent::Resized(physical_size) => {
+						state.resize(*physical_size);
+					}
+					WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+						state.resize(**new_inner_size);
+					}
+					_ => {}
+				}
+			}
+			Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+				state.update();
+				match state.render() {
+					Ok(_) => {}
+					// Reconfigure the surface if lost
+					Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+					// The system is out of memory, we should probably quit
+					Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+					// All other errors (Outdated, Timeout) should be resolved by the next frame
+					Err(e) => eprintln!("{:?}", e),
+				}
+			}
+			Event::MainEventsCleared => {
+				// RedrawRequested will only trigger once unless we manually
+				// request it.
+				state.window().request_redraw();
+			}
+			_ => {}
+		}
+	});
+}
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+	render_pipeline: wgpu::RenderPipeline,
+    // The window must be declared after the surface so
+    // it gets dropped after it as the surface contains
+    // unsafe references to the window's resources.
+    window: Window,
+
+}
+
+impl State {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+        
+        // # Safety
+        //
+        // The surface needs to live as long as the window that created it.
+        // State owns the window, so this should be safe.
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+
+		let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web, we'll have to disable some.
+                limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                label: None,
+            },
+            None, // Trace path
+        ).await.unwrap();
+
+		let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps.formats.iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &config);
+
+		let modes = &surface_caps.present_modes;
+
+		let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+			label: Some("Shader"),
+			source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+		});
+
+		let render_pipeline_layout =
+			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+			label: Some("Render Pipeline Layout"),
+			bind_group_layouts: &[],
+			push_constant_ranges: &[],
+		});
+
+		let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+			label: Some("Render Pipeline"),
+			layout: Some(&render_pipeline_layout),
+			vertex: wgpu::VertexState {
+				module: &shader,
+				entry_point: "vs_main", // 1.
+				buffers: &[], // 2.
+			},
+			fragment: Some(wgpu::FragmentState { // 3.
+				module: &shader,
+				entry_point: "fs_main",
+				targets: &[Some(wgpu::ColorTargetState { // 4.
+					format: config.format,
+					blend: Some(wgpu::BlendState::REPLACE),
+					write_mask: wgpu::ColorWrites::ALL,
+				})],
+			}),
+			primitive: wgpu::PrimitiveState {
+				topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+				strip_index_format: None,
+				front_face: wgpu::FrontFace::Ccw, // 2.
+				cull_mode: Some(wgpu::Face::Back),
+				// Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+				polygon_mode: wgpu::PolygonMode::Fill,
+				// Requires Features::DEPTH_CLIP_CONTROL
+				unclipped_depth: false,
+				// Requires Features::CONSERVATIVE_RASTERIZATION
+				conservative: false,
+			},
+			depth_stencil: None, // 1.
+			multisample: wgpu::MultisampleState {
+				count: 1, // 2.
+				mask: !0, // 3.
+				alpha_to_coverage_enabled: false, // 4.
+			},
+			multiview: None, // 5.
+		});
+		
+		Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+			render_pipeline,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+	pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+		if new_size.width > 0 && new_size.height > 0 {
+			self.size = new_size;
+			self.config.width = new_size.width;
+			self.config.height = new_size.height;
+			self.surface.configure(&self.device, &self.config);
+		}
+	}
+
+	pub fn input(&mut self, event: &WindowEvent, control_flow: &mut ControlFlow) -> bool {
+		return match event {
+			WindowEvent::CursorMoved {position, ..} => {println!("{:?}", position); true}
+			WindowEvent::CloseRequested => {*control_flow = ControlFlow::Exit; true},
+			WindowEvent::KeyboardInput { input, .. } => {self.keyboard_input(input, control_flow)}
+			_ => false
+		};
+	}
+
+	pub fn keyboard_input(&mut self, input: &KeyboardInput, control_flow: &mut ControlFlow) -> bool {
+		if input.state == ElementState::Pressed{
+			return match input.virtual_keycode {
+				Some(VirtualKeyCode::Escape) => {*control_flow = ControlFlow::Exit; true}
+				Some(VirtualKeyCode::Space) => {}
+				_ => {println!("{:?}", input); false}
+			}
+		}
+		false
+	}
+
+
+    fn update(&mut self) {
+        
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+		let output = self.surface.get_current_texture()?;
+		let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+		let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+			label: Some("Render Encoder"),
+		});
+
+		{
+			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				label: Some("Render Pass"),
+				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+					view: &view,
+					resolve_target: None,
+					ops: wgpu::Operations {
+						load: wgpu::LoadOp::Clear(wgpu::Color {
+							r: 0.1,
+							g: 0.2,
+							b: 0.3,
+							a: 1.0,
+						}),
+						store: wgpu::StoreOp::Store,
+					},
+				})],
+				depth_stencil_attachment: None,
+				occlusion_query_set: None,
+				timestamp_writes: None,
+			});
+			render_pass.set_pipeline(&self.render_pipeline); // 2.
+    		render_pass.draw(0..3, 0..1); // 3.
+		}
+	
+		// submit will accept anything that implements IntoIter
+		self.queue.submit(std::iter::once(encoder.finish()));
+		output.present();
+	
+		Ok(())
+    }
+}
+ 
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use Minefactory::run;
+fn main() {
+    pollster::block_on(run());
+}
+ 
